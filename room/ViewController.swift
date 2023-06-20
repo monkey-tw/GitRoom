555871d7b3f00795ba7251273f7003a7dec806f2
Jiaxin Pu <jiaxin.pu@thoughtworks.com>
2023-06-20 17:35:01 +0800

    first commit

---
+++ room/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  room
+//
+//  Created by Jiaxin Pu on 2023/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ roomUITests/roomUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  roomUITestsLaunchTests.swift
+//  roomUITests
+//
+//  Created by Jiaxin Pu on 2023/6/20.
+//
+
+import XCTest
+
+final class roomUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
